344ff6d9596799b77c3c2f548eb27cc380ec1f36
frekerzzzz <kannamugesh135@gmail.com>
2023-11-07 20:18:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,123 @@
-import './App.css';
-import Card from './ReactCard/Card';
+import "./App.css";
+import Card from "./ReactCard/Card";
 
+const cardDetails = [
+  {
+    valid: "FREE",
+    plan: 0,
+    feature: [
+      {
+        key: "Single User",
+        value: true,
+      },
+      {
+        key: "5GB Storage",
+        value: true,
+      },
+      {
+        key: "Unlimited Public Projects",
+        value: true,
+      },
+      {
+        key: "Community Access",
+        value: false,
+      },
+      {
+        key: "Unlimited Private Projects",
+        value: false,
+      },
+      {
+        key: "Dedicated Phone Support",
+        value: false,
+      },
+      {
+        key: "Free Subdomain",
+        value: false,
+      },
+      {
+        key: "Monthly Status Reports",
+        value: false,
+      },
+    ],
+  },
+  {
+    valid: "PLUS",
+    plan: 9,
+    feature: [
+      {
+        key: "5 User",
+        value: true,
+      },
+      {
+        key: "50GB Storage",
+        value: true,
+      },
+      {
+        key: "Unlimited Public Projects",
+        value: true,
+      },
+      {
+        key: "Community Access",
+        value: true,
+      },
+      {
+        key: "Dedicated Phone Support",
+        value: true,
+      },
+      {
+        key: "Free Subdomain",
+        value: true,
+      },
+      {
+        key: "Monthly Status Reports",
+        value: false,
+      },
+    ],
+  },
+  {
+    valid: "PRO",
+    plan: 49,
+    feature: [
+      {
+        key: "Unlimited User",
+        value: true,
+      },
+      {
+        key: "150GB Storage",
+        value: true,
+      },
+      {
+        key: "Unlimited Public Projects",
+        value: true,
+      },
+      {
+        key: "Community Access",
+        value: true,
+      },
+      {
+        key: "Dedicated Phone Support",
+        value: true,
+      },
+      {
+        key: "Free Subdomain",
+        value: true,
+      },
+      {
+        key: "Monthly Status Reports",
+        value: true,
+      },
+    ],
+  },
+];
 function App() {
   return (
     <div className="App">
-     <Card />
+      <h1> Price card </h1>
+      <div className="top-content">
+        {cardDetails.map((detail) => {
+          return <Card details={detail}></Card>;
+        })}
+      </div>
     </div>
   );
 }
--- src/ReactCard/Card.jsx
+++ src/ReactCard/Card.jsx
@@ -1,83 +1,34 @@
 import React from 'react';
 import "./Card.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 
-const Card = () => {
+const Card = ({ details }) => {
   return (
     <div>
-        <div className='head'>
-        <h1> Price card </h1>
+
+      <div className='container'>
+        <div className='cards'>
+          <div className='header'>
+            <p>{details.valid}</p>
+            <h2>${details.plan}<small>/month</small></h2>
+          </div>
+
+          <div className='card-body'>
+            <ul>
+              {details.feature.map((item) => {
+                return <li className={item.value ? '' : 'false-value'}><span>{item.value ?
+                  <FontAwesomeIcon icon={faCheck} /> :
+                  <FontAwesomeIcon icon={faXmark} />}
+                </span>{item.key}</li>
+              })}
+            </ul>
+          </div>
+          <div className='btn'>
+            <button>BUTTON</button>
+          </div>
         </div>
-
-        <div className='container'>
-            <div className='cards'>
-                <div className='header'>
-                <p>FREE</p>
-                <h1>$0/month</h1>
-                </div>
-
-                <div className='card-body'>
-                <ul>
-                    <li><span>&#10003;</span>Single User</li>
-                    <li><span>&#10003;</span>50GB Storage</li>
-                    <li><span>&#10003;</span>Unlimited Public Projects</li>
-                    <li><span>&#10003;</span>Community Access</li>
-                    <li className='headerSpan'><span>&#9747;</span>Unlimited Private Projects</li>
-                    <li className='headerSpan'><span>&#9747;</span>Dedicated Phone Support</li>
-                    <li className='headerSpan'><span>&#9747;</span>Free Subdomain</li>
-                    <li className='headerSpan'><span>&#9747;</span>Monthly Status Reports</li>
-                </ul>
-                </div>
-                <div className='btn'>
-                <button>BUTTON</button>
-                </div>               
-            </div>
-
-               <div className='cards'>
-                <div className='header'>
-                <p>PLUS</p>
-                <h1>$9/month</h1>
-                </div>
-
-                <div className='card-body'>
-                <ul>
-                    <li><span>&#10003;</span>5 User</li>
-                    <li><span>&#10003;</span>50GB Storage</li>
-                    <li><span>&#10003;</span>Unlimited Public Projects</li>
-                    <li><span>&#10003;</span>Community Access</li>
-                    <li><span>&#9747;</span>Unlimited Private Projects</li>
-                    <li><span>&#9747;</span>Dedicated Phone Support</li>
-                    <li><span>&#9747;</span>Free Subdomain</li>
-                    <li className='headerSpan'><span>&#9747;</span>Monthly Status Reports</li>
-                </ul>
-                </div>
-                <div className='btn'>
-                <button>BUTTON</button>
-                </div>
-                            </div>
-
-               <div className='cards'>
-                <div className='header'>
-                <p>PRO</p>
-                <h1>$49/month</h1>
-                </div>
-
-                <div className='card-body'>
-                 <ul>
-                    <li><span>&#10003;</span>Single User</li>
-                    <li><span>&#10003;</span>50GB Storage</li>
-                    <li><span>&#10003;</span>Unlimited Public Projects</li>
-                    <li><span>&#10003;</span>Community Access</li>
-                    <li><span>&#9747;</span>Unlimited Private Projects</li>
-                    <li><span>&#9747;</span>Dedicated Phone Support</li>
-                    <li><span>&#9747;</span>Free Subdomain</li>
-                    <li><span>&#9747;</span>Monthly Status Reports</li>
-                </ul>
-                </div>
-                <div className='btn'>
-                <button>BUTTON</button>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
   )
 }
